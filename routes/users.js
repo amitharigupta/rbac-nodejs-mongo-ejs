c4fcb899b564722c843664425b0eec4a27f77a76
amitharigupta <amitgt9967@gmail.com>
2023-09-19 09:48:47 +0530

    changes done for pages and css

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/profile',async (req, res, next) => {
-  res.send('User Profile Called');
+router.get("/profile", async (req, res, next) => {
+  const person = req.user;
+  res.render("profile", { title: "Profile Page", person });
 });
 
 module.exports = router;
